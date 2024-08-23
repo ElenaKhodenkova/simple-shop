fd0f496a1fd14b8353e2b6b53bc7c4652f171aae
ElenaKhodenkova <novavselena1980@gmail.com>
2024-08-23 10:58:18 +0300

    DeleteButton

--- server/src/routers/cart.router.js
+++ server/src/routers/cart.router.js
@@ -15,12 +15,8 @@ router.get('/cart', verifyAccessToken, async (req, res) => {
 
 router.post('/cart', verifyAccessToken, async (req, res) => {
   const { productId } = req.body;
-  
   const { user } = res.locals;
-  
   const cart = await Cart.findOne({ where: { productId, userId: user.id } });
-  
-
   if (cart) {
     cart.count += 1;
     await cart.save();
@@ -30,20 +26,39 @@ router.post('/cart', verifyAccessToken, async (req, res) => {
   res.json({ mesage: 'Товар добавлен в корзину' });
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', verifyAccessToken, async (req, res) => {
   const { user } = res.locals;
   try {
     const cart = await Cart.findAll({
       include: [{ model: Product }],
       where: { userId: req.params.id },
     });
-    
-
     res.json(cart);
   } catch (error) {
     console.error(error);
     res.sendStatus(400);
   }
 });
+
+
+router.delete('/:id', verifyAccessToken, async (req, res) =>{
+  const {id} = req.params;
+  const { user } = res.locals;
+try {
+  const product = await Cart.findOne({where: { productId: id }})
+  console.log("---------------", product);
+  if (product.userId === user.id) {
+    product.destroy();
+    res.sendStatus(200);
+  } else{
+    res.status(400).json({message: "У вас нет прав на удаление"});
+  }
+} catch (error) {
+  console.error(error);
+  res.sendStatus(400);
+}
+})
+
+
 
 module.exports = router;
